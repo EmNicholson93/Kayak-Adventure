4883754f3cb5e321f4ec01c05eb17f3a428baafc
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-09 13:04:05 -0700

    pausing to work on group project mid function

--- test/section-completed.test.js
+++ test/section-completed.test.js
@@ -10,8 +10,16 @@ test('create span for completing a section', assert => {
     const expected = '<span class="section completed">The Glorious Green Truss!</span>';
     
     //act
-    const dom = createSectionLink(section);
+    const dom = sectionCompleted(section);
 
     //assert
     assert.equal(dom.outerHTML, expected);
 });
+
+function sectionCompleted(section) {
+    const span = document.createElement('span');
+
+    const span.classList.add('section');
+    const span.classList.add('completed');
+    
+}
